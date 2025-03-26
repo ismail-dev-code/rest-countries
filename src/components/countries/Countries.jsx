ca20dfe976d4c2bb4c5d3a255be2cb267ae31107
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-27 00:41:28 +0600

    updated layout

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,21 +1,22 @@
-import React, { use } from 'react';
-import Country from './country/Country';
-import './components/countries.css'
+import React, { use } from "react";
+import Country from "./country/Country";
+import "../countries.css";
 
+const Countries = ({ countriesPromise }) => {
+  const countries = use(countriesPromise);
 
-const Countries = ({countriesPromise}) => {
-    const countries = use(countriesPromise);
-    
-   
-    return (
-        <div className='countries'>
-            <h1><small>Total Countries:</small> {countries.length}</h1>
-            {
-        countries.map(country=> <Country key={country.cca3} country={country}></Country>)
-    }
-        </div>
-    );
-   
+  return (
+    <div>
+      <h1>
+        <small>Total Countries:</small> {countries.length}
+      </h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Countries;           
+export default Countries;
--- src/components/countries/country/Country.jsx
+++ src/components/countries/country/Country.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './country.css'
+import React from "react";
+import "./country.css";
 
-const Country = ({country}) => {
-
-    return (
-        <div className='country'>
-            <h3>Name: {country.name.common}</h3>
-            <img src={country.flags.png} alt="img loading" />
-            <p>Freedom Status: {country.independent? "Independent" : "Not Independent"}</p>
-            <p>Population: {country.population}</p>
-        </div>
-    );
+const Country = ({ country }) => {
+  return (
+    <div className="country">
+      <h3>Name: {country.name.common}</h3>
+      <img src={country.flags.png} alt="img loading" />
+      <p>
+        Freedom Status:{" "}
+        {country.independent ? "Independent" : "Not Independent"}
+      </p>
+      <p>Population: {country.population}</p>
+    </div>
+  );
 };
 
 export default Country;
