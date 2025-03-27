4ef64dcdcd20786134d2dd331c67c81101701173
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-27 11:16:19 +0600

    visited countries function added

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,18 +1,29 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "./country/Country";
 import "../countries.css";
 
 const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
   const countries = use(countriesPromise);
+
+  const handleVisitedCountries = (country) => {
+    console.log("country visited to be added hoice", country);
+  };
 
   return (
     <div>
       <h1>
         <small>Total Countries:</small> {countries.length}
       </h1>
+      <h3>Traveled so far: </h3>
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country
+            key={country.cca3}
+            handleVisitedCountries={handleVisitedCountries}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/countries/country/Country.jsx
+++ src/components/countries/country/Country.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries}) => {
+  const [visited, setVisited] = useState(false);
+  // console.log(handleVisitedCountries);
+
+  const handleClick = () => {
+    // if (visited === true) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    setVisited(!visited);
+    handleVisitedCountries(country);
+  };
   return (
-    <div className="country">
+    <div className={`country ${visited && "country-visited"}`}>
       <h3>Name: {country.name.common}</h3>
       <img src={country.flags.png} alt="img loading" />
       <p>
@@ -11,6 +23,9 @@ const Country = ({ country }) => {
         {country.independent ? "Independent" : "Not Independent"}
       </p>
       <p>Population: {country.population}</p>
+      <button className={visited? "btn-visited": "btn-not-visited"} onClick={handleClick}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
